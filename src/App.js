43bf7490bdb74950263d53ff9bfa70d60534d695
RATHINAKUMAR SUNDARAJAN <srathinakumar91@gmail.com>
2024-05-27 15:50:08 +0530

    changed app

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
+        {/* <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} /> */}
       </Routes>
       <Toaster />
     </BrowserRouter>
